d6d6d047a74b054e73138ff9216dfa793703ae20
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-21 17:49:14 +0530

    [Shruti] Perform binary search program

--- DataStructAlgorithmPrograms_244/Program.cs
+++ DataStructAlgorithmPrograms_244/Program.cs
@@ -7,7 +7,18 @@ namespace DataStructAlgorithmPrograms_244
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure Algorithm Problems");
-            
+
+            BinarySearch bs = new BinarySearch();
+            string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+            Console.WriteLine("Please enter a string to be searched :");
+            string key = Console.ReadLine();
+            int result = bs.Binary_Search(arr, key);
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at " + "index " + result);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
