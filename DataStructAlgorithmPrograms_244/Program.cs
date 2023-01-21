c03ffc5a59a258d4bafa574578354a23aca3fc27
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-21 18:01:36 +0530

    [Shruti] Perform bubble sort program

--- DataStructAlgorithmPrograms_244/Program.cs
+++ DataStructAlgorithmPrograms_244/Program.cs
@@ -8,16 +8,20 @@ namespace DataStructAlgorithmPrograms_244
         {
             Console.WriteLine("Data Structure Algorithm Problems");
 
-            BinarySearch bs = new BinarySearch();
-            string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
-            Console.WriteLine("Please enter a string to be searched :");
-            string key = Console.ReadLine();
-            int result = bs.Binary_Search(arr, key);
-            if (result == -1)
-                Console.WriteLine("Element not present");
-            else
-                Console.WriteLine("Element found at " + "index " + result);
-            Console.WriteLine(result);
+            //BinarySearch bs = new BinarySearch();
+            //string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+            //Console.WriteLine("Please enter a string to be searched :");
+            //string key = Console.ReadLine();
+            //int result = bs.Binary_Search(arr, key);
+            //if (result == -1)
+            //    Console.WriteLine("Element not present");
+            //else
+            //    Console.WriteLine("Element found at " + "index " + result);
+            //Console.WriteLine(result);
+            //Console.ReadLine();
+
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.CheckBubbleSort();
             Console.ReadLine();
         }
     }
