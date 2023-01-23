4f3c51ad04e5fc4fc2b6ab41a90f328bf66076f0
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-23 17:20:36 +0530

    [Shruti] Perform Merge sort program

--- DataStructAlgorithmPrograms_244/AnagramDetection.cs
+++ DataStructAlgorithmPrograms_244/AnagramDetection.cs
@@ -6,32 +6,32 @@ using System.Threading.Tasks;
 
 namespace DataStructAlgorithmPrograms_244
 {
-    internal class AnagramDetection
-    {
-        public static bool Anagram(string word1, string word2)
-        {
-            //Convert Word To Char Array
-            char[] s1 = word1.ToCharArray();
-            char[] s2 = word2.ToCharArray();
-            //Sort Two Array Words
-            Array.Sort(s1);
-            Array.Sort(s2);
+    //internal class AnagramDetection
+    //{
+    //    public static bool Anagram(string word1, string word2)
+    //    {
+    //        //Convert Word To Char Array
+    //        char[] s1 = word1.ToCharArray();
+    //        char[] s2 = word2.ToCharArray();
+    //        //Sort Two Array Words
+    //        Array.Sort(s1);
+    //        Array.Sort(s2);
 
-            string val1 = new string(s1);
-            string val2 = new string(s2);
+    //        string val1 = new string(s1);
+    //        string val2 = new string(s2);
 
-            if (val1 == val2)//Check Val1=val2
-            {
-                //If True Then Show true;
-                Console.WriteLine("This Is Anagram");
-                return true;
-            }
-            else
-            {
-                //Else False
-                Console.WriteLine("This Is Not Anagram");
-                return false;
-            }
-        }
-    }
+    //        if (val1 == val2)//Check Val1=val2
+    //        {
+    //            //If True Then Show true;
+    //            Console.WriteLine("This Is Anagram");
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            //Else False
+    //            Console.WriteLine("This Is Not Anagram");
+    //            return false;
+    //        }
+    //    }
+    //}
 }
--- DataStructAlgorithmPrograms_244/Program.cs
+++ DataStructAlgorithmPrograms_244/Program.cs
@@ -4,36 +4,73 @@ namespace DataStructAlgorithmPrograms_244
 {
     internal class Program
     {
+        static void Merge(int[] arr, int low, int mid, int high)
+        {
+            int[] tempArr = new int[arr.Length];
+
+            int left1 = 0;
+            int temp = 0;
+
+            int total = 0;
+            int loop = 0;
+
+            left1 = (mid - 1);
+            temp = low;
+            total = (high - low + 1);
+
+            while ((low <= left1) && (mid <= high))
+            {
+                if (arr[low] <= arr[mid])
+                    tempArr[temp++] = arr[low++];
+                else
+                    tempArr[temp++] = arr[mid++];
+            }
+            while (low <= left1)
+                tempArr[temp++] = arr[low++];
+            while (mid <= high)
+                tempArr[temp++] = arr[mid++];
+
+            for (loop = 0; loop < total; loop++)
+            {
+                arr[high] = tempArr[high];
+                high--;
+            }
+
+        }
+        static void Sort(int[] arr, int low, int high)
+        {
+            int mid = 0;
+
+            if (high > low)
+            {
+                mid = (high + low) / 2;
+
+                Sort(arr, low, mid);
+                Sort(arr, (mid + 1), high);
+
+                Merge(arr, low, (mid + 1), high);
+
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Data Structure Algorithm Problems");
 
-            //BinarySearch bs = new BinarySearch();
-            //string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
-            //Console.WriteLine("Please enter a string to be searched :");
-            //string key = Console.ReadLine();
-            //int result = bs.Binary_Search(arr, key);
-            //if (result == -1)
-            //    Console.WriteLine("Element not present");
-            //else
-            //    Console.WriteLine("Element found at " + "index " + result);
-            //Console.WriteLine(result);
-            //Console.ReadLine();
+            int[] arr = { 32, 18, 27, 65, 12, 41, 22, 26, 44 };
+
+            int length = 0;
+            int loop = 0;
+
+            length = arr.Length;
+
+            Sort(arr, 0, length - 1);
+
+            Console.WriteLine("Sorted Array:");
+            for (loop = 0; loop < arr.Length; loop++)
+                Console.Write(arr[loop] + " ");
+            Console.WriteLine();
 
 
-            //BubbleSort bubbleSort = new BubbleSort();
-            //bubbleSort.CheckBubbleSort();
-            //Console.ReadLine();
-
-
-            //Console.WriteLine("Enter The First Word");
-            //string word1 = Console.ReadLine();
-            //Console.WriteLine("Enter The Second Word");
-            //string word2 = Console.ReadLine();
-            //AnagramDetection.Anagram(word1, word2);//Pass Values To Function
-            //Console.ReadLine();
-
-            PrimeNumber.Prime();
         }
     }
 }
